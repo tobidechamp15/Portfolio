49ca5fd0e266ec41adb90f855836c74b5e8f3c6f
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2024-07-29 17:17:15 +0100

    NavBar created

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,11 @@
+import NavBar from "@/components/NavBar";
 import React from "react";
+import "./globals.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className="bg-[#1A1A1A]  h-screen">
+      <NavBar />
       <div>Page</div>
     </div>
   );
---
+++ components/NavBar.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React, { useState } from "react";
+import "../app/globals.css"; // Correct path to your global CSS
+
+const NavBar = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <div className="flex justify-between xsm:px-[30px] md:justify-evenly items-center py-[30px]">
+      <span className="logoStyle">Tobiloba</span>
+      <section className="md:flex hidden nav-items">
+        <span>Home</span>
+        <span>About</span>
+        <span>Project</span>
+        <span>Services</span>
+        <span>Resume</span>
+      </section>
+      <section className="contact-item hidden md:flex ">Contact Me</section>
+      <div
+        className={`hamburgerMenu flex md:hidden ${isActive ? "active" : ""}`}
+        onClick={toggleMenu}
+      >
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
