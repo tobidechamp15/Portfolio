c449d98cf219483021ca5bf1a02705b7a2610e5d
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2024-07-30 22:27:32 +0100

    Feat Contact

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import NavBar from "@/components/NavBar";
 import React from "react";
 import "./globals.css";
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import avatar from "../public/assets/avatar.svg";
 
 const Hero = () => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView({
+    threshold: 0.2, // Adjust this value to determine how much of the element should be in view before the animation starts
+  });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
+  const heroVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+  };
+
   return (
-    <div className="container min-h-screen items-center justify-center xsm:pt-[50px] xsm:mb-[190px] flex flex-col bg-hero-bg bg-no-repeat bg-cover bg-center xsm:px-[25px]">
-      <Image src={avatar} />
+    <motion.div
+      ref={ref}
+      initial="hidden"
+      animate={controls}
+      variants={heroVariants}
+      className="container min-h-screen items-center justify-center xsm:pt-[50px] xsm:mb-[190px] flex flex-col bg-hero-bg bg-no-repeat bg-cover bg-center xsm:px-[25px]"
+    >
+      <Image src={avatar} alt="Avatar" />
       <span className="hero-name text-center">Oluwadare Oluwatobiloba</span>
       <span className="job-title">Software Engineer</span>
       <span className="job-des text-center">
@@ -15,10 +41,13 @@ const Hero = () => {
         challenges and delivering impactful solutions that enhance user
         experiences and streamline operations.
       </span>
-      <span className="font-[Montserrat] my-3 border text-white hover:text-black cursor-pointer border-[#4FC3F7] py-[18px] px-[50px] rounded-[75px] transition duration-300 ease-in-out transform hover:bg-[#4FC3F7] hover:scale-105">
+      <motion.span
+        whileHover={{ scale: 1.05, backgroundColor: "#4FC3F7", color: "#000" }}
+        className="font-[Montserrat] my-3 border text-white cursor-pointer border-[#4FC3F7] py-[18px] px-[50px] rounded-[75px] transition duration-300 ease-in-out"
+      >
         Contact Me
-      </span>
-    </div>
+      </motion.span>
+    </motion.div>
   );
 };
 
